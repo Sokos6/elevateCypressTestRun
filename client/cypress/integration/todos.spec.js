1ec79641438e9cc4ad1182cc2644adf5bdaec8da
Will Sokolowski <will.sokolowski@gmail.com>
2019-06-03 13:28:04 -0700

    initial testing

--- client/cypress/integration/todos.spec.js
+++ client/cypress/integration/todos.spec.js
@@ -1,10 +1,26 @@
+const serverUrl = Cypress.env('serverUrl');
+
 describe('todo app', () => {
     beforeEach(() => {
+
+        cy.fixture('todos/all_before.json').as('todosJSON');
+        cy.fixture('todos/add.json').as('addTodoJSON');
+        cy.fixture('todos/all_after.json').as('updatedJSON');
+
+        cy.server();
+        cy.route('GET', `${serverUrl}/todos`, '@todosJSON').as('getAllTodos');
+
         cy.visit('/');
+        cy.wait('@getAllTodos');
         cy.get('h1').contains('Todo List');
     });
 
-    it('should display the todo list', () => {});
+    it('should display the todo list', () => {
+        // network stubs
+        cy.server();
+        cy.route('GET', `${serverUrl}/todos`, '@updatedJSON').as('getAllTodos');
+        cy.route('POST', `${serverUrl}/todos`, '@addTodoJSON').as('addTodo');
+    });
 
     it('should add a new todo to the list', () => {});
 
